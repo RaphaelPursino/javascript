1dbe9444890d2b6aecd48d827f0ba274b8d45072
RaphaelPursino <107816621+RaphaelPursino@users.noreply.github.com>
2022-07-18 20:25:49 -0300

    javascript

---
+++ Aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { //condição simples
+    console.log('Voce ultrapassou a velocidade permitida')
+}
+console.log('Dirija sempre usando o cinto de segurança')
